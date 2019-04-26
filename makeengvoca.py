8580e3f20cc968fd5b378aa7c102d04e33bc1482
Yoovin <soo22839@gmail.com>
2019-04-27 03:59:18 +0900

    make execution file for makeengvoca.py and exception handling

--- makeengvoca.py
+++ makeengvoca.py
@@ -1,29 +1,37 @@
 import random
 
-def makeEngVoca(vocafile, quesNum): # 인자로 단어파일(str)과 문항수(int)를 받음
-    vocaTxt = open(vocafile, 'r')
-    vocaword = open("vocaword.txt", 'w')
-    vocamean = open("vocamean.txt", 'w')
-    wordList = []
-    meanList = []
+def makeEngVoca(vocafile, quesNum, vocaword, vocamean): # 인자로 단어파일(str), 문항수(int), 만들어질 단어장파일이름(str), 단어정답파일이름(str)을 받음
 
-    for i in range(10):
-        (word, space, mean) = vocaTxt.readline().partition(' ')
-        wordList.append(word)
-        meanList.append(mean[:-1])
+    try:
+        if vocaword == '':
+            vocaword = 'vocaword'
 
+        if vocamean == '':
+            vocamean = 'vocamean'
 
-    randomQuesNumber = random.sample(range(0,len(wordList)), quesNum)
+        vocaTxt = open(vocafile + 'txt', 'r')
+        vocaWord = open(vocaword + 'txt', 'w')
+        vocaMean = open(vocamean + 'txt', 'w')
+        wordList = []
+        meanList = []
 
-    for j in randomQuesNumber:
-        vocaword.write(wordList[j] + '\n')
-        vocamean.write(wordList[j] + ' ' + meanList[j] + '\n')
+        for i in range(10):
+            (word, space, mean) = vocaTxt.readline().partition(' ')
+            wordList.append(word)
+            meanList.append(mean[:-1])
 
-    vocaTxt.close()
-    vocaword.close()
-    vocamean.close()
-    return wordList, meanList
+        if quesNum > len(wordList):
+            return "만들려는 문항수가 단어 수보다 많습니다!"
 
-wordList, meanList = makeEngVoca('voca.txt', 5)
-print(wordList)
-print(meanList)
+        randomQuesNumber = random.sample(range(0,len(wordList)), quesNum)
+
+        for j in randomQuesNumber:
+            vocaWord.write(wordList[j] + '\n')
+            vocamean.write(wordList[j] + ' ' + meanList[j] + '\n')
+
+        vocaTxt.close()
+        vocaWord.close()
+        vocaMean.close()
+        return "제작 완료"
+    except FileNotFoundError:
+        return vocafile + "이름을 가진 단어 파일이 없습니다!"
