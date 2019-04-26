7a28e51418062ad3f13ac82393f9ae2a367fba69
Yoovin <soo22839@gmail.com>
2019-04-27 07:38:23 +0900

    make .exe file for execution no python

---
+++ sourcefile/makeengvoca.py
@@ -0,0 +1,43 @@
+import random
+import codecs
+
+def makeEngVoca(vocafile, quesNum, vocaword, vocamean): # 인자로 단어파일(str), 문항수(int), 만들어질 단어장파일이름(str), 단어정답파일이름(str)을 받음
+
+    try:
+        index = 1
+
+        if vocaword == vocamean:
+            return "단어시험지와 정답의 파일이름이 같으면 안됩니다!"
+
+        vocaTxt = codecs.open(vocafile, 'r', encoding='utf8')
+        vocaWord = codecs.open(vocaword, 'w', encoding='utf8')
+        vocaMean = codecs.open(vocamean, 'w', encoding='utf8')
+        wordList = []
+        meanList = []
+
+        while True:
+            (word, space, mean) = vocaTxt.readline().partition(' ')
+            wordList.append(word)
+            meanList.append(mean[:-1])
+
+            if wordList[-1] == '':
+                wordList = wordList[:-1]
+                break
+
+        if quesNum > len(wordList):
+            return "만들려는 문항수가 단어 수보다 많습니다!"
+
+        randomQuesNumber = random.sample(range(0,len(wordList)), quesNum)
+
+        for j in randomQuesNumber:
+            vocaWord.write(str(index) + '. ' + wordList[j] + '\n')
+            vocaMean.write(str(index) + '. ' + wordList[j] + ' ' + meanList[j] + '\n')
+            index += 1
+
+        vocaTxt.close()
+        vocaWord.close()
+        vocaMean.close()
+        return "제작 완료"
+
+    except FileNotFoundError:
+        return vocafile + "이름을 가진 단어 파일이 없습니다!"
