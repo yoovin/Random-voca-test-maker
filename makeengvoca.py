ec50e35a014c275d7bbfdb90f6b7ccca08651bce
Yoovin <soo22839@gmail.com>
2019-04-27 02:49:42 +0900

    make it can read flie, divide words and means

--- makeengvoca.py
+++ makeengvoca.py
@@ -4,5 +4,26 @@ def makeEngVoca(vocatxt):
     voca = str(vocatxt)
     vocaTxt = open(voca, 'r')
 
-    vocaTxt.
+t = open("voca.txt", 'r')
+vocaword = open("vocaword.txt", 'w')
+vocamean = open("vocamean.txt", 'w')
 
+
+wordList = []
+meanList = []
+for i in range(5):
+    (word, space, mean) = t.readline().partition(' ')
+    wordList.append(word)
+    meanList.append(mean[:-1])
+
+for j in range(len(wordList)):
+    vocaword.write(wordList[j] + '\n')
+    vocamean.write(wordList[j] + ' ' + meanList[j] + '\n')
+
+print(wordList)
+print(meanList)
+
+print(t.readline())
+t.close()
+vocaword.close()
+vocamean.close()
